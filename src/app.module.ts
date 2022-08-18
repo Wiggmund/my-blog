693eee07e01858dac9a1ffa40975b578f0c12a22
wiggmund <40165054+Wiggmund@users.noreply.github.com>
2022-08-18 17:24:34 +0300

    Added main modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+import { ReactionsModule } from './reactions/reactions.module';
+import { RolesModule } from './roles/roles.module';
+import { HashtagsModule } from './hashtags/hashtags.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PostsModule,
+    UsersModule,
+    ReactionsModule,
+    RolesModule,
+    HashtagsModule
+  ],
   controllers: [],
   providers: [],
 })
